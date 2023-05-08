e29cc9ecf702dbb3f96d0534922a7321c34c5d27
Aleksandr <abyzovaleksandr5@gmail.com>
2023-05-08 18:51:43 +0500

    feat: add folder files-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
 
+const path = require('node:path');
+const fs = require('fs');
+
+const folder = path.resolve(__dirname, 'files');
+const copyFolder = path.resolve(__dirname, 'files-copy');
+
+const promis = fs.promises;
+
+async function copyDir() {
+    await promis.rm(copyFolder, {
+        recursive: true,
+    }).catch(() => { });
+
+    await promis.mkdir(copyFolder, {
+        recursive: true,
+    }).catch(() => { });
+
+    await promis.readdir(folder).then((files) => {
+        files.forEach((f) => {
+            fs.copyFile(path.join(folder, f), path.join(copyFolder, f), () => { })
+        })
+    }).catch(() => { })
+}
+
+
+copyDir();
