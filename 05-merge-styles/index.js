1a7b4e58f432296f57cd192e5e681057d940a391
Aleksandr <abyzovaleksandr5@gmail.com>
2023-05-10 23:07:53 +0500

    fix: fix synchronous functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,52 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const path = require('path');
+
+// const stylesPath = path.join(__dirname, 'styles');
+// const distPath = path.join(__dirname, 'project-dist/bundle.css');
+
+// fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
+//     if (err) throw err;
+//     const fileCSS = files.filter(f => path.extname(f.name) === '.css');
+
+//     if (!fs.existsSync(path.join(__dirname, 'project-dist'))) {
+//         fs.mkdirSync(path.join(__dirname, 'project-dist'))
+//     }
+//     const strWrite = fs.createWriteStream(distPath);
+
+//     fileCSS.forEach((file) => {
+//         const cssPath = path.join(stylesPath, file.name);
+//         const strRead = fs.createReadStream(cssPath);
+//         strRead.on('data', data => strWrite.write(data));
+//         strRead.on('error', error => process.stdout.write('error', error.message))
+//     })
+// })
+const promis = require('node:fs/promises');
 const path = require('path');
 
-const stylesPath = path.join(__dirname, 'styles');
-const distPath = path.join(__dirname, 'project-dist/bundle.css');
+const application = async () => {
+  try {
+    const stylesPath = path.resolve(__dirname,'styles');
+    const distPath = path.resolve(__dirname,'project-dist');
 
-fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    const fileCSS = files.filter(f => path.extname(f.name) === '.css');
+    const Abcontroller = new AbortController();
+    const { signal } = Abcontroller;
 
-    if (!fs.existsSync(path.join(__dirname, 'project-dist'))) {
-        fs.mkdirSync(path.join(__dirname, 'project-dist'))
+    const files = await promis.readdir(stylesPath, { withFileTypes: true });
+
+    let result = [];
+
+    for (const item of files) {
+      if (item.isFile() && path.extname(item.name) === '.css') {
+        const data = await promis.readFile(path.join(stylesPath, item.name), { encoding: 'utf8' });
+
+        result.push(data);
+      }
     }
-    const strWrite = fs.createWriteStream(distPath);
 
-    fileCSS.forEach((file) => {
-        const cssPath = path.join(stylesPath, file.name);
-        const strRead = fs.createReadStream(cssPath);
-        strRead.on('data', data => strWrite.write(data));
-        strRead.on('error', error => process.stdout.write('error', error.message))
-    })
-})
+    await promis.writeFile(path.join(distPath, 'bundle.css'), result, { signal });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+application();
